483ed7fd571605eec08d07f4013ee7d78906a2b8
mlj <mlj@gmail.com>
2021-01-05 18:44:24 +0800

    手写EventBus

--- app/src/main/java/com/docker/handwrite/MainActivity.java
+++ app/src/main/java/com/docker/handwrite/MainActivity.java
@@ -1,17 +1,33 @@
 package com.docker.handwrite;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.docker.handwrite.handeventbus.EventBusActivity;
 
 /**
  * @author docker
  */
 public class MainActivity extends AppCompatActivity {
 
+    private Button mEventBusBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEventBusBtn = findViewById(R.id.event_bus_btn);
+        //1.handEventBus
+        mEventBusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EventBusActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
